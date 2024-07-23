9df5e7509bbc4c4d1cc2ab58fec8c0b8f42252a7
JeromeChallet <jeromepchallet@gmail.com>
2024-07-23 09:41:00 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -20,8 +25,21 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return <li>{item.description}</li>;
+}
+
 function Stats() {
   return (
     <footer className="stats">
