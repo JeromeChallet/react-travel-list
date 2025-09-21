6358ddc148485c0dfce78d3041c2516ce9071467
JeromeChallet <jeromepchallet@gmail.com>
2025-09-21 11:08:49 +0900

    Do lesson 71

--- src/App.js
+++ src/App.js
@@ -1,5 +1,62 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Charger", quantity: 1, packed: false },
+];
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h2>What do you need for your 😍 trip?</h2>
+    </div>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>🧳 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
 }
 
 export default App;
